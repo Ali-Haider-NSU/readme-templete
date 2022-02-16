5f11222b6edc67b2d336b4be497b2f272f2e0d74
Ali-Haider-NSU <mdalihaider800@gmail.com>
2022-02-16 12:36:53 +0600

    readme template

---
+++ README.md
@@ -0,0 +1,69 @@
+<!--markdown tutorial-->
+Ali Haider<br/>
+This is second line
+---
+# Ali Haider
+## Ali Haider
+### Ali Haider
+#### Ali Haider
+##### Ali Haider
+###### Ali Haider
+
+<p>
+This is Ali haider. I am 25 years old. I just finished my bachelor degree from NSU.
+</p>
+
+<i>This and italic</i>  
+_This is italic with markdown_
+
+__This is Bold__
+
+<del>This is delete</del>  
+~~This is delete in markdown~~
+
+'For code'  
+```html
+<html>
+<head> </head>
+<body> </body>
+</html>
+```
+
+```javascript
+console.log('Hello')
+
+```
+### Ordered list
+<ol>
+<li> Item 1</li>
+<li> Item 2</li>
+<li> Item 3</li>
+</ol>
+
+For markdown language  
+1. Item one
+2. Item two
+3. Item three 
+
+### Unordered list
+- Item 1
+- Item 2
+- Item 3
+
+### Automatic link
+http://www.google.com
+
+### Disable link
+`http://www.google.com`
+
+### Markdown link Syntax
+[Google](http://www.google.com)
+
+### Image syntax
+![profile](./image/profile1.jpg)
+
+### Table syntax
+
+| Name | Email |
+| ----- | ------ | 
+| Ali |alihaider@gmail.com |
---
+++ day2.js
@@ -0,0 +1,57 @@
+
+// alert("hello");
+// document.write("Ali Haider<br/>");
+// document.write("01521318062<br/>");
+// document.write("Bangladesh<br/>");
+
+// var x = "Ali Haider<br/>";
+// document.write(x);
+// x ="Kolpona";
+// document.write(x);
+
+// var num =20;
+// num = toString(num);
+// document.write(typeof(num));
+// document.write("<br/>")
+
+// var num="20";
+// num=parseInt(num);
+// document.write(typeof(num ));
+// document.write("<br/>")
+
+// var num="20.5";
+// num=parseFloat(num);
+// document.write(typeof(num));
+// document.write("<br/>")
+
+// var num =2.1212;
+// document.write(num.toFixed(1));
+
+// document.write("Ali"+" Haider"+" Dipu <br/>");
+
+// var fname="Ali ";
+// var lname="Haider";
+// var fullname=fname+lname;
+// document.write("My name is : "+fullname);
+
+// var text="Bangladesh";
+// document.write("No of caracter "+text.length);
+
+// var text=prompt("Enter your name ");
+// document.write("No of caracter "+text.length);
+
+// var text="Bangladesh";
+// document.write(text.charAt(2));
+// document.write("<br/> "+text.toUpperCase());
+// document.write("<br/>"+text.toLocaleLowerCase());
+
+// var t1="Ali";
+// var t2=" Haider";
+// document.write("<br/>"+t1.concat(t2)+"<br/>");
+
+// document.write(text.slice(0,3));
+
+
+
+
+
